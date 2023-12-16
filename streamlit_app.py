a971a77531e0e1bd22644d35d4abd2e2ff5ab73c
limotalel <107612749+limotalel@users.noreply.github.com>
2023-12-16 13:05:28 +0300

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-choice = st.sidebar.selectbox("select your choice",["Summarise Document"])
-
-if choice == "Summarise Text":
-    st.subheader("Summarise text using txtai")
-    input_text=st.text_area("Enter you text here")
-elif choice =="Summarise Document":
-    st.subheader("Summarise Document using txtai")
